b91bff129330931fb0ad15005bd094ca7898d594
Your Name <you@example.com>
2024-09-12 16:54:28 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import Header from "./components/Header"
+
+import Genre from "./components/Genre"
+import FilmDesc from "./components/FilmDesc"
+import Rating from "./components/Rating"
+import ButtonBlock from "./components/ButtonBlock"
+import Popular from "./components/Popular"
+import AgeMark from "./components/AgeMark"
+function App() {
+  let filmTitle = 'Stranger Things';
+  let filmDesc = 'In 1980s Indiana, a group of young friends witness supernatural forces and secret government exploits. As they search for answers, the children unravel a series of extraordinary mysteries.'
+  let countOfStars = '5';
+  let genres = [
+    {
+      value: 'Drama │',
+    },
+    {
+      value: 'Thriller │',
+    },
+    {
+      value: 'Supernatural'
+    }
+  ];
+  return (
+    <div className="container">
+    <Header></Header>
+     <div className="main-content">
+      <Genre genreValue = {genres}></Genre>
+      <FilmDesc title = {filmTitle} desc = {filmDesc}></FilmDesc>
+      <Rating rating = {countOfStars}></Rating>
+      <ButtonBlock></ButtonBlock>
+      <Popular></Popular>
+     </div>
+     <AgeMark></AgeMark>
+    </div>
+     
+
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import HeaderLog from "./HeaderLog";
+import logo from "../assets/netflix-logo.png";
+import "./Header.scss";
+
+import searchIcon from "../assets/search-icon.png";
+const Header = () => {
+  // const today = new Date();
+  // const month = today.getMonth();
+  // const year = today.getFullYear();
+  // const date = today.getDate();
+  // if (date == 1 && date == 21 && date == 31){
+  //   const currentDate = month + "/" + date + "st";
+  // }
+  // if (date == 2){
+  //   const currentDate = month + "/" + date + "nd";
+  // }
+  // if (date == 3){
+  //   const currentDate = month + "/" + date + "rd";
+  // }
+  // else{
+  //   const currentDate = month + "/" + date + "th";
+  // }
+  const currentDate = "Friday 8th July";
+  let [showState, setShowState] = useState(false);
+  function changeShowState() {
+    setShowState((showState) => !showState);
+  }
+  return (
+    <header className="header-container">
+      <div className="header-container__left">
+        <img className="header-container__left__logo" src={logo} alt="logo" />
+        <div className="header-container__left__date">| {currentDate}</div>
+      </div>
+      <div className="header-container__right">
+        {showState ? (
+          <input className="header-container__right__input" type="text" />
+        ) : (
+          <div></div>
+        )}
+        <button onClick={changeShowState} className="search">
+          <img
+            className="header-container__right__search"
+            src={searchIcon}
+            alt="search"
+          ></img>
+        </button>
+
+        <HeaderLog></HeaderLog>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
